6842079f8426d31021a192e1f85a4475d9ba9591
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-07-13 12:29:07 +0200

    Update create server using find free port app

--- 09.http.js
+++ 09.http.js
@@ -1,12 +1,20 @@
 const http = require("node:http");
+const { findAvailablePort } = require("./10.free-port");
 // El servidor, básicamente recibe una solicitud y devuelve una respuesta
 const server = http.createServer((req, res) => {
   console.log("Request retrieved"), res.end("Hello world!");
 });
-// PARA MODO DESARROLLO, EN PPRODUCCIÓN HAY QUE TENER CONTROL DEL PUERTO
+/* // PARA MODO DESARROLLO, EN PPRODUCCIÓN HAY QUE TENER CONTROL DEL PUERTO
 server.listen(0, () => {
   // El puerto 0 encuentra el primero disponible
   console.log(
     `Server listening on port http://localhost:${server.address().port}`
   );
+}); */
+// Usando nuestra app para encontrar puerto libre
+
+findAvailablePort(1234).then((port) => {
+  server.listen(port, () => {
+    console.log(`Server listening on port http://localhost:${port}`);
+  });
 });
