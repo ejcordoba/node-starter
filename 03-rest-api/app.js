b18d3377ea4f16590b2497158c6dc041b3ec4157
Eduardo Córdoba <eduardo.cordoba.j@gmail.com>
2024-07-14 09:11:58 +0200

    Add basic POST endpoint

--- 03-rest-api/app.js
+++ 03-rest-api/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const crypto = require("node:crypto");
 const movies = require("./movies.json");
 
 const app = express();
+app.use(express.json()); // Middleware
 app.disable("x-powered-by");
 
 app.get("/", (req, res) => {
@@ -9,6 +11,13 @@ app.get("/", (req, res) => {
 });
 
 app.get("/movies", (req, res) => {
+  const { genre } = req.query;
+  if (genre) {
+    const filteredMovies = movies.filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLowerCase())
+    );
+    return res.json(filteredMovies);
+  }
   res.json(movies);
 });
 
@@ -17,6 +26,24 @@ app.get("/movies/:id", (req, res) => {
   const movie = movies.find((movie) => movie.id === id);
   if (movie) return res.json(movie);
   res.status(404).json({ message: "Movie not found" });
+});
+
+app.post("/movies", (req, res) => {
+  const { title, year, director, duration, poster, genre, rate } = req.body;
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    year,
+    director,
+    duration,
+    poster,
+    genre,
+    rate: rate ?? 0,
+  };
+  // Esto no sería REST, porque estamos guardando
+  // el estado de la aplicación en memoria
+  movies.push(newMovie);
+  res.status(201).json(newMovie); // Puede ser interesante devolver la info para actualizar cache del cliente
 });
 
 const PORT = process.env.PORT ?? 1234;
